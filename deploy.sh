4112a1e73f588a877057c7eeabfa11b478bf4830
nyakamwe <nyakamweaimable@gmail.com>
2023-08-15 12:21:21 +0200

    Update portfolio site

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,16 @@ rm -rf .output
 # Generate the static site
 npm run generate
 
-# Copy to deployment branch
+# Clean the deploy branch
 git checkout deploy
+git rm -r .
+git clean -df
+
+# Copy the generated files to the root directory
 cp -r .output/public/* .
 git add .
 git commit -m "Update portfolio site"
 git push origin deploy
 
 # Switch back to the main branch
-git checkout master
+git checkout main
