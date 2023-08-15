e8a29bc34f11dd673c8adf3b275da5e24b3ad269
nyakamwe <nyakamweaimable@gmail.com>
2023-08-15 12:35:14 +0200

    add deploy shell script and automate deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clean output directory
+rm -rf .output
+
+# Generate the static site
+npm run generate
+
+# Clean the deploy branch
+git checkout deploy
+git rm -r .
+git clean -df
+
+# Copy the generated files to the root directory
+cp -r .output/public/* .
+git add .
+git commit -m "Update portfolio site"
+git push origin deploy
+
+# Switch back to the main branch
+git checkout main
