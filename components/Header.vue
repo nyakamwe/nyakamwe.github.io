c07ec6337004b0088ac3f457a85fecedce8ed29b
nyakamwe <nyakamweaimable@gmail.com>
2024-08-02 16:23:16 +0200

    feat: improve landing page content, and remove offers navigation link in header

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
             <div class="flex items-center">
                 <NuxtLink to="/" exact-active-class="exact-active" class="mt-2 mx-2">Home</NuxtLink>
                 <NuxtLink to="/work" exact-active-class="exact-active" class="mt-2 mx-2">Endeavors</NuxtLink>
-                <NuxtLink to="/services" exact-active-class="exact-active" class="mt-2 mx-2">Offers</NuxtLink>
+                <!-- <NuxtLink to="/services" exact-active-class="exact-active" class="mt-2 mx-2">Offers</NuxtLink> -->
             </div>
             <img class="w-12 h-12 rounded-full" src="https://avatars.githubusercontent.com/u/53573955?s=400&u=15719f1c24e8edb176d3dba0f98c0c907bd541d1&v=4" alt="profile-image">
         </nav>
